7096c3d060f7ed86c876b78633d9bff3e898f602
Kanishq Sodhani <virusbaba017@gmail.com>
2026-01-15 00:03:54 +0530

    sql page created

---
+++ Frontend/src/pages/SQL/SqlHome.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/layout/Navbar";
+
+const SqlHome = () => {
+  return (
+    <section>
+      <Navbar />
+      <div>{/* <h1>sql home page</h1> */}</div>
+    </section>
+  );
+};
+
+export default SqlHome;
---
+++ Frontend/src/pages/SQL/SqlTopic.jsx
@@ -0,0 +1,9 @@
+const SqlTopic = () => {
+  return (
+    <div>
+      <h1>SQL Topics - Intro, CRUD, Joins</h1>
+    </div>
+  );
+};
+
+export default SqlTopic;
